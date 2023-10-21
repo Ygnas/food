49eee5d0114a30ffa626c1b7869cbea2fdad4314
Ygnas <ygnas@outlook.com>
2023-10-21 15:02:53 +0100

    File and image helper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
