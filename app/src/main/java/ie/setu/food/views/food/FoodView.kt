33ec1c1678916071788ec85877a67a85592cf01d
Ygnas <ygnas@outlook.com>
2023-10-21 14:47:20 +0100

    Food view

---
+++ app/src/main/java/ie/setu/food/views/food/FoodView.kt
@@ -0,0 +1,60 @@
+package ie.setu.food.views.food
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import ie.setu.food.R
+import ie.setu.food.databinding.ActivityFoodBinding
+import ie.setu.food.models.FoodModel
+import timber.log.Timber.i
+
+class FoodView : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFoodBinding
+    private lateinit var presenter: FoodPresenter
+    var food = FoodModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityFoodBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.toolbarAdd.title = title
+        setSupportActionBar(binding.toolbarAdd)
+
+        presenter = FoodPresenter(this)
+
+
+
+        binding.btnAdd.setOnClickListener {
+            if (binding.placemarkTitle.text.toString().isEmpty()) {
+                Snackbar.make(binding.root, R.string.enter_food_title, Snackbar.LENGTH_LONG)
+                    .show()
+            } else {
+                presenter.doAddOrSave(binding.placemarkTitle.text.toString(), binding.placemarkDescription.text.toString())
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_food, menu)
+        val deleteMenu: MenuItem = menu.findItem(R.id.item_delete)
+        deleteMenu.isVisible = presenter.edit
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_delete -> {
+                presenter.doDelete()
+            }
+            R.id.item_cancel -> {
+                presenter.doCancel()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
