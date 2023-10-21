bd707217f96d077e651e19dbf44e6527aef0c89d
Ygnas <ygnas@outlook.com>
2023-10-21 14:42:35 +0100

    Main app

---
+++ app/src/main/java/ie/setu/food/main/MainApp.kt
@@ -0,0 +1,21 @@
+package ie.setu.food.main
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ie.setu.food.models.FoodJSONStore
+import ie.setu.food.models.FoodMemStore
+import ie.setu.food.models.FoodStore
+import timber.log.Timber
+import timber.log.Timber.i
+
+class MainApp : Application() {
+
+    lateinit var foods: FoodStore
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        foods = FoodMemStore()
+        i("Food started")
+    }
+}
