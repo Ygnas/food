f4e150d09606c0c5f7cfd889f847ff1ebab750f1
Ygnas <ygnas@outlook.com>
2023-10-21 21:42:30 +0100

    New gallery view and layout

---
+++ app/src/main/java/ie/setu/food/views/gallery/GalleryView.kt
@@ -0,0 +1,12 @@
+package ie.setu.food.views.gallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ie.setu.food.R
+
+class GalleryView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery_view)
+    }
+}
