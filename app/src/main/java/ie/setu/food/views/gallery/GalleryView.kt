6104bded7004e630e931f6445b9bd8f9738c034f
Ygnas <ygnas@outlook.com>
2023-10-22 11:06:56 +0100

    Displayed gallery title

--- app/src/main/java/ie/setu/food/views/gallery/GalleryView.kt
+++ app/src/main/java/ie/setu/food/views/gallery/GalleryView.kt
@@ -2,11 +2,12 @@ package ie.setu.food.views.gallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.recyclerview.widget.GridLayoutManager
+import ie.setu.food.R
 import ie.setu.food.databinding.ActivityGalleryViewBinding
 import ie.setu.food.main.MainApp
 import ie.setu.food.models.FoodModel
-import ie.setu.food.views.foodlist.FoodListener
 
 class GalleryView : AppCompatActivity(), ImageListener {
 
@@ -17,12 +18,18 @@ class GalleryView : AppCompatActivity(), ImageListener {
         super.onCreate(savedInstanceState)
         binding = ActivityGalleryViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.toolbargallery.title = title
         presenter = GalleryPresenter(this)
         app = application as MainApp
 
         val layoutManager = GridLayoutManager(this,3)
         binding.galleryrecycler.layoutManager = layoutManager
         loadImages()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
     private fun loadImages() {
