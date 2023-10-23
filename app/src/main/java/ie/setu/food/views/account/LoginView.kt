83f00cdeb851634082d0f83318b53bd8104a5e57
Ygnas <ygnas@outlook.com>
2023-10-23 20:51:33 +0100

    Login view

---
+++ app/src/main/java/ie/setu/food/views/account/LoginView.kt
@@ -0,0 +1,12 @@
+package ie.setu.food.views.account
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ie.setu.food.R
+
+class LoginView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_view)
+    }
+}
