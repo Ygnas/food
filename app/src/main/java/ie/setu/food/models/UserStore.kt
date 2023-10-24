2120134c80f52e1b0a3286e83dcdfd5fbb846380
Ygnas <ygnas@outlook.com>
2023-10-24 18:35:12 +0100

    User models

---
+++ app/src/main/java/ie/setu/food/models/UserStore.kt
@@ -0,0 +1,9 @@
+package ie.setu.food.models
+
+interface UserStore {
+    fun findAll(): List<UserModel>
+    fun findById(id:Long): UserModel?
+    fun create(user: UserModel)
+    fun update(user: UserModel)
+    fun delete(user: UserModel)
+}
