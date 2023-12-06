055a5d9ddd5eba795ce1dcc98f13cfdae919eb75
Ygnas <ygnas@outlook.com>
2023-12-06 19:16:24 +0000

    FoodList Fragment

--- app/src/main/java/ie/setu/food/ui/foodlist/FoodListFragment.kt
+++ app/src/main/java/ie/setu/food/ui/foodlist/FoodListFragment.kt
@@ -1,24 +1,55 @@
 package ie.setu.food.ui.foodlist
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ie.setu.food.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import ie.setu.food.databinding.FragmentFoodListBinding
+import ie.setu.food.models.FoodModel
+import ie.setu.food.views.foodlist.FoodAdapter
+import ie.setu.food.views.foodlist.FoodListener
+import timber.log.Timber.i
 
-class FoodListFragment : Fragment() {
+class FoodListFragment : Fragment(), FoodListener {
 
-    companion object {
-        fun newInstance() = FoodListFragment()
-    }
-
+    private lateinit var binding: FragmentFoodListBinding
     private lateinit var viewModel: FoodListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_food_list, container, false)
+    ): View {
+        binding = FragmentFoodListBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this)[FoodListViewModel::class.java]
+        binding.recyclerView.layoutManager = LinearLayoutManager(activity)
+
+        viewModel.observableFoodList.observe(viewLifecycleOwner) { foods ->
+            foods?.let {
+                render(foods as ArrayList<FoodModel>)
+            }
+        }
+
+        return binding.root
+    }
+
+    private fun render(foodList: ArrayList<FoodModel>) {
+        binding.recyclerView.adapter = FoodAdapter(foodList, this)
+        if (foodList.isEmpty()) {
+            binding.recyclerView.visibility = View.GONE
+//            binding.donationsNotFound.visibility = View.VISIBLE
+        } else {
+            i(foodList.toString())
+            binding.recyclerView.visibility = View.VISIBLE
+//            binding.donationsNotFound.visibility = View.GONE
+        }
+    }
+
+    override fun onFoodClick(food: FoodModel, position: Int) {
+        val action = FoodListFragmentDirections.actionNavHomeToFoodFragment(food)
+        findNavController().navigate(action)
     }
 }
--- app/src/main/java/ie/setu/food/ui/foodlist/FoodListViewModel.kt
+++ app/src/main/java/ie/setu/food/ui/foodlist/FoodListViewModel.kt
@@ -1,7 +1,27 @@
 package ie.setu.food.ui.foodlist
 
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseUser
+import ie.setu.food.firebase.FirebaseAuthentication
+import ie.setu.food.firebase.FirebaseDB
+import ie.setu.food.models.FoodModel
 
 class FoodListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val foodList = MutableLiveData<List<FoodModel>>()
+    val observableFoodList: LiveData<List<FoodModel>>
+        get() = foodList
+
+    var liveFirebaseUser = MutableLiveData<FirebaseUser>()
+
+    init {
+//        FirebaseDB.create(liveFirebaseUser)
+        load()
+    }
+
+    fun load() {
+        FirebaseDB.findAll(foodList)
+    }
 }
